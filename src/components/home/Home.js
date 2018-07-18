2751343339c3adc43a2972b2c4a06d67934ba33b
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-07-18 16:34:42 +0100

    Update writing page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../shared/button/Button';
 
 import './Home.css';
 import profile from './profile.png';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <section className="about">
-        <img
-          src={profile}
-          alt="Profile"
-        />
+export default () => (
+  <section className="home grid__container">
+    <img
+      src={profile}
+      alt="Profile"
+    />
 
-        <div>
-          <h1>
-            I
-            <span className="highlight"> build </span>
-            software for the <span className="highlight">web</span>
-          </h1>
+    <div className="intro__wrapper">
+      <h1>
+        I
+        <span className="highlight"> build </span>
+        software for the <span className="highlight">web</span>
+      </h1>
 
-          <p>
-            Hi there
-            <span role="img" aria-label="waving hand"> 👋🏾</span>, my name is
-            Chinedu Daniel. I’m a generalist developer building a specialty on
-            the frontend. Welcome to my little space on the web.
-          </p>
+      <p>
+        Hi there
+        <span role="img" aria-label="waving hand"> 👋🏾</span>, my name is
+        Chinedu Daniel. I’m a generalist developer building a specialty on
+        the frontend. Welcome to my little space on the web.
+      </p>
 
-          <p>
-            I'm currently with
-            <a href="https://andela.com" target="_blank">Andela</a> building
-            <a href="https://gobble.com" target="_blank">useful products</a>
-            for humans.
-          </p>
+      <p>
+        I'm currently with
+        <a href="https://andela.com" target="_blank">Andela</a> building
+        <a href="https://gobble.com" target="_blank">useful products</a>
+        for humans.
+      </p>
 
-          <p>
-            I love the open source culture and during my free time, I try to
-            <a href="https://github.com/blueyedgeek" target="_blank">
-            give back</a> to the open source community.
-          </p>
+      <p>
+        I love the open source culture and during my free time, I try to
+        <a href="https://github.com/blueyedgeek" target="_blank">
+        give back</a> to the open source community.
+      </p>
 
-          <Button
-            href={'mailto:chinedudaniel7@gmail.com?subject=Hi+Chinedu'}
-            text='Say Hi'
-          />
-        </div>
-      </section>
-    );
-  }
-}
+      <Button
+        href={'mailto:chinedudaniel7@gmail.com?subject=Hi+Chinedu'}
+        text='Say Hi'
+      />
+    </div>
+  </section>
+);
--- src/components/writing/Writing.js
+++ src/components/writing/Writing.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-export default class Writing extends Component {
-  render() {
-    return [
-      <header key="writing--header">
-        header stuff goes here
-      </header>,
+import notepad from './notepad.svg';
 
-      <main key="writing--main">
-        Articles go here
-      </main>
-    ];
-  }
-}
+import './Writing.css'
+
+export default () => (
+  <section className="writing grid__container">
+    <img
+      src={notepad}
+      alt="Notepad"
+    />
+
+    <div className="intro__wrapper">
+      <h1>
+        <span className="highlight">Writing</span>
+      </h1>
+
+      <p>
+        Welcome to my writers' corner where I get to write about anything I want
+        (mostly technical articles but sometimes other thoughts).
+      </p>
+    </div>
+
+    <section className="posts__container">
+      <article className="post">
+        <h2 className="post-title">
+          Hello World 🌎
+          <div className="post-date">
+            18 July, 2018
+          </div>
+        </h2>
+      </article>
+    </section>
+  </section>
+);
