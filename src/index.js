88fcb096aa8955835e6e29f8412fbed570941bb3
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-07-19 14:15:41 +0100

    Add react snap for static pre-rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -13,5 +13,11 @@ const App = () => (
   </BrowserRouter>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 registerServiceWorker();
