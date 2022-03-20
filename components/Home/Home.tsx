6c465aefe2e3624187f0419ff092db997e590354
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-03-20 10:09:07 +0100

    Work on intro secton of homepage

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -14,7 +14,7 @@ const Home: FC = () => {
       </Head>
       <nav className={styles.navWrapper}>
         <Link href="/">
-          <a>Chinedu Daniel</a>
+          <a className={styles.logo}>CD</a>
         </Link>
 
         <div className={styles.navLinksWrapper}>
@@ -28,23 +28,84 @@ const Home: FC = () => {
       </nav>
 
       <main className={styles.main}>
-        <p>Psst, my name is</p>
+        <section className={styles.intro}>
+          <p>
+            Psst <span className={styles.wave}>👋🏾</span>my name is
+          </p>
 
-        <h1>Chinedu Daniel</h1>
-        <h2>I build software for the web</h2>
+          <h1>Chinedu Daniel</h1>
+          <h2>I build software for the web</h2>
 
-        <p>
-          I am a software developer with a penchant for building great
-          experiences on the web. Welcome to my own slice of the internet.
-        </p>
+          <p>
+            I am a software developer with a penchant for building great
+            experiences on the web. Welcome to my own slice of the internet.
+          </p>
 
-        <p>
-          I currently work at{" "}
-          <a href="https://forto.com" target="_blank" rel="noreferrer">
-            Forto
-          </a>{" "}
-          building useful products for humans.
-        </p>
+          <p>
+            I currently work at{" "}
+            <a href="https://forto.com" target="_blank" rel="noreferrer">
+              Forto
+            </a>{" "}
+            building useful products for humans.
+          </p>
+
+          <a href="mailto:chinedudaniel7@gmail.com?subject=Hi+Chinedu">
+            Say Hi
+          </a>
+        </section>
+
+        <section>
+          <div>
+            <p>8th March, 2020</p>
+            <h3>Reimplementing slice in pure JavaScript</h3>
+
+            <p>
+              The slice method in JavaScript is a useful utility method, when it
+              is called on an array, it will return a shallow copy of a part of
+              that array…
+            </p>
+          </div>
+          <div>
+            <p>8th March, 2020</p>
+            <h3>Reimplementing slice in pure JavaScript</h3>
+
+            <p>
+              The slice method in JavaScript is a useful utility method, when it
+              is called on an array, it will return a shallow copy of a part of
+              that array…
+            </p>
+          </div>
+          <div>
+            <p>8th March, 2020</p>
+            <h3>Reimplementing slice in pure JavaScript</h3>
+
+            <p>
+              The slice method in JavaScript is a useful utility method, when it
+              is called on an array, it will return a shallow copy of a part of
+              that array…
+            </p>
+          </div>
+          <div>
+            <p>8th March, 2020</p>
+            <h3>Reimplementing slice in pure JavaScript</h3>
+
+            <p>
+              The slice method in JavaScript is a useful utility method, when it
+              is called on an array, it will return a shallow copy of a part of
+              that array…
+            </p>
+          </div>
+          <div>
+            <p>8th March, 2020</p>
+            <h3>Reimplementing slice in pure JavaScript</h3>
+
+            <p>
+              The slice method in JavaScript is a useful utility method, when it
+              is called on an array, it will return a shallow copy of a part of
+              that array…
+            </p>
+          </div>
+        </section>
       </main>
     </Container>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
           crossOrigin="crossorigin"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Allan:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
