c919eb2293bf613c622cab09dd9d77a03c9edefe
Chinedu Daniel <chinedudaniel7@gmail.com>
2024-11-14 20:12:08 +0100

    Show draft posts in development (#71)

--- utils/posts.ts
+++ utils/posts.ts
@@ -13,7 +13,10 @@ const postsDirectory = path.join(process.cwd(), "posts");
 // https://nextjs.org/learn/basics/data-fetching/implement-getstaticprops
 export const getSortedPostsData = (dir = postsDirectory) => {
   // Get file names under /posts
-  const fileNames = getNonDraftFiles(dir);
+  const fileNames =
+    process.env.NODE_ENV === "development"
+      ? getAllFiles(dir)
+      : getNonDraftFiles(dir);
   const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, "");
@@ -48,7 +51,10 @@ export const getSortedPostsData = (dir = postsDirectory) => {
 
 // https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticpaths
 export function getAllPostPaths() {
-  const fileNames = getNonDraftFiles(postsDirectory);
+  const fileNames =
+    process.env.NODE_ENV === "development"
+      ? getAllFiles(postsDirectory)
+      : getNonDraftFiles(postsDirectory);
 
   return fileNames.map((fileName) => {
     const [year, month, date, ...slug] = fileName.split("-");
@@ -82,4 +88,8 @@ const getMarkdownContent = (content: string) => content.split("---")[1]; // remo
 const getNonDraftFiles = (dir: string) => {
   const fileNames = fs.readdirSync(dir);
   return fileNames.filter((fileName) => !fileName.startsWith("draft"));
+};
+
+const getAllFiles = (dir: string) => {
+  return fs.readdirSync(dir);
 };
